5fc7c080ffb5fb684cb0e840da91c0685e3154ed
abneralvesmuniz76@gmail.com <abneralvesmuniz76@gmail.com>
2023-11-15 17:26:54 -0300

    Atualizando projeto

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,17 +1,19 @@
 import React from "react";
 import './Header.css'
+import logoNet from "../../assets/NetflixLogo.png"
+import fotoAbner from "../../assets/Abner.jpg"
 
 const Header = ({efeito}) => {
     return (
         <header className={efeito ? 'Efeito' : ''}>
             <div className="logoNet">
                 <a href="/">
-                    <img src="./public/NetflixLogo.png" alt="" />  
+                    <img src={logoNet} alt="" />  
                 </a>
             </div>
             <div className="Usuario">
                 <a href="/">
-                    <img src="./public/Abner.jpg" alt="" />
+                    <img src={fotoAbner} alt="" />
                 </a>
 
             </div>
